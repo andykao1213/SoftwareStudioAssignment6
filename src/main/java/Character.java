46d857c18a2298e4b1a4b979b15d2b8c1db53917
jyChou <a23064569@gmail.com>
2016-05-09 21:22:34 +0800

    display nodes and nodes undragged animation

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -1,5 +1,8 @@
 package main.java;
 
+import java.awt.Color;
+import java.util.ArrayList;
+
 import processing.core.PApplet;
 
 /**
@@ -9,15 +12,43 @@ import processing.core.PApplet;
 public class Character {
 	
 	private MainApplet parent;
-
-	public Character(MainApplet parent){
+	public int row , col;
+	public int rad = 45;
+	public float x , y;
+	public float originX , originY;
+	public boolean reseting = false;
+	public boolean inCircle = false;
+	private String clr;
+	private int color;
+	private String name;
+	private ArrayList<Character> targets;
+	private ArrayList<Character> values;
+	public Character(MainApplet parent, String name, String clr ,int row , int col  ){
 
 		this.parent = parent;
+		this.row = row;
+		this.col = col;
+		this.clr = clr;
+		this.originX = col*60 + 40;
+		this.originY = row*60 + 60;
+		this.x = originX;
+		this.y = originY;
+		this.color = (int) Long.parseLong(clr.replace("#", ""), 16);
+		this.inCircle = false;
+		this.targets = new ArrayList<Character>();
 		
 	}
 
 	public void display(){
-
+		
+		this.parent.noStroke();
+		parent.fill(color, 200);
+		parent.ellipse(x, y, rad, rad);
 	}
 	
+	
+	public void addTarget(Character target){ this.targets.add(target); }
+	public void addValue(Character value){ this.targets.add(value); }
+	public ArrayList<Character> getTargets(){ return this.targets; }
+	public ArrayList<Character> getValues(){ return this.values; }
 }
--- src/main/java/MainApplet.java
+++ src/main/java/MainApplet.java
@@ -1,6 +1,10 @@
 package main.java;
 
+import java.util.ArrayList;
+
 import processing.core.PApplet;
+import processing.data.JSONArray;
+import processing.data.JSONObject;
 
 /**
 * This class is for sketching outcome using Processing
@@ -8,8 +12,13 @@ import processing.core.PApplet;
 */
 @SuppressWarnings("serial")
 public class MainApplet extends PApplet{
+	
 	private String path = "main/resources/";
 	private String file = "starwars-episode-1-interactions.json";
+	private Character dragNode;
+	JSONObject data;
+	JSONArray nodes, links;
+	private ArrayList<Character> characters = new ArrayList<Character>();
 	
 	private final static int width = 1200, height = 650;
 	
@@ -22,11 +31,68 @@ public class MainApplet extends PApplet{
 	}
 
 	public void draw() {
-
+		background(255);
+		for(Character character: this.characters){
+			if(character.reseting == true) this.nodeReset(character);
+			character.display();
+		}
 	}
-
+	
+	public void mousePressed() {
+		
+		for(Character character: this.characters){
+			if( dist(character.x , character.y , mouseX , mouseY) < character.rad / 2){
+				character.rad = 40;
+				dragNode = character;
+			}
+			else character.rad = 45;	
+			
+		}
+			
+	}
+	public void mouseReleased() {
+		for(Character character: this.characters){
+			 character.rad = 45;
+			 if(character.inCircle == false){
+					 character.reseting = true;
+			 }
+		}
+		dragNode = null;
+	}
+	public void mouseDragged() {
+		if (dragNode != null) {
+			dragNode.x = mouseX;
+			dragNode.y = mouseY;
+		}
+	}
 	private void loadData(){
+		
+		data = loadJSONObject(path + file);
+		nodes = data.getJSONArray("nodes");
+		links = data.getJSONArray("links");
 
+		System.out.println("Number of nodes: " + nodes.size());
+		System.out.println("Number of links: " + links.size());
+
+		for(int i=0; i<nodes.size(); i++){
+			JSONObject node = nodes.getJSONObject(i);
+			characters.add(new Character(this , node.getString("name") , node.getString("colour") , i %10 , i /10));
+		}
+
+		for(int i=0; i<links.size(); i++){
+			JSONObject link = links.getJSONObject(i);
+			characters.get(link.getInt("source")).addTarget(characters.get(link.getInt("target")));
+			characters.get(link.getInt("source")).addTarget(characters.get(link.getInt("value")));
+		}
 	}
-
+	private void nodeReset(Character c){
+		
+			c.x = c.originX + (c.x - c.originX)* (float)0.9;
+			c.y = c.originY + (c.y - c.originY)* (float)0.9;
+			if(c.x - c.originX < 0.01 || c.y - c.originY < 0.01){
+				c.x = c.originX;
+				c.y = c.originY;
+				c.reseting = false;
+			}
+	}
 }
